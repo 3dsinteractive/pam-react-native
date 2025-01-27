b2af96e4e0fb79331b13dca403a7a763c7d911b5
Heart <narongrit@pams.ai>
2025-01-27 08:22:50 +0700

    update code

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Button } from 'react-native';
 import { Pam } from 'pam-react-native';
 
 Pam.initialize({
@@ -13,6 +13,19 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Result:</Text>
+      <Button
+        title="CLICK"
+        onPress={async () => {
+          console.log('clicked');
+          try {
+            console.log('CALL TRACK');
+            const result = await Pam.track('click', { name: 'button' });
+            console.log('RESULT', result);
+          } catch (e) {
+            console.log('ERROR', e);
+          }
+        }}
+      />
     </View>
   );
 }
