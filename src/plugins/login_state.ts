f027d61fac64519a779d85b6b582664a21e842ca
Heart <narongrit@pams.ai>
2025-03-10 23:08:06 +0700

    fix(userLogin): handle login when no customer's profile yet

--- src/plugins/login_state.ts
+++ src/plugins/login_state.ts
@@ -4,8 +4,8 @@ import { type Plugin } from '../core/plugin';
 export class LoginState implements Plugin {
   initPlugin(pam: PamTracker): void {
     pam.hook.onPreTracking('*', (p) => {
-      const contactId = pam.contactState!.getContactId();
-      const database = pam.contactState!.getDatabase();
+      const contactId = pam.contactState?.getContactId();
+      const database = pam.contactState?.getDatabase();
 
       if (!database) {
         // When database is empty cancel the event tracking
@@ -13,29 +13,37 @@ export class LoginState implements Plugin {
         return p;
       }
 
-      if (contactId) {
+      if (p.form_fields._contact_id === '<REMOVE>') {
+        delete p.form_fields._contact_id;
+      } else if (contactId) {
         p.form_fields._contact_id = contactId;
       }
-      p.form_fields._database = database;
+      if (!p.form_fields._database) {
+        p.form_fields._database = database;
+      }
 
       return p;
     });
 
     pam.hook.onPostTracking('*', (payload, result) => {
       if (payload.event === 'logout') {
-        pam.contactState!.logout();
+        pam.contactState?.logout();
       } else if (payload.event === 'login') {
-        const loginKey = pam.contactState!.getLoginKey();
-        const loginId = payload.form_fields[loginKey];
-        pam.contactState!.login(loginId);
+        if (result && pam.config.loginDBAlias === result._database) {
+          const loginKey = pam.contactState?.getLoginKey();
+          if (loginKey) {
+            const loginId = payload.form_fields[loginKey];
+            pam.contactState?.login(loginId);
+          }
+        }
       }
-      if (result?.contact_id) {
-        pam.contactState!.setContactId(result.contact_id);
+      if (result && result.contact_id) {
+        pam.contactState?.setContactId(result.contact_id);
       }
     });
 
     pam.hook.onClean(async (_) => {
-      pam.contactState!.clean();
+      pam.contactState?.clean();
     });
   }
 }
